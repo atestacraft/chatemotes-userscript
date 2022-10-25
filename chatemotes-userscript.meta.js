dd85e50ed45264d29ab54c31082fd1d167b65ec7
crashmax-dev <crashmax.dev@gmail.com>
2022-10-25 10:06:58 +0000

    Deploying to gh-pages from @ atestacraft/chatemotes-userscript@779346b3d10d4fe3ee9281c62f3c16a094e1a207 🚀

---
+++ chatemotes-userscript.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        chatemotes-userscript
+// @version     0.0.0
+// @license     MIT
+// @homepage    https://atestacraft.github.io/chatemotes-userscript/
+// @match       *://7tv.app/*
+// @match       *://betterttv.com/*
+// @match       *://www.frankerfacez.com/*
+// @grant       GM_addStyle
+// @updateURL   https://atestacraft.github.io/chatemotes-userscript/chatemotes-userscript.meta.js
+// @downloadURL https://atestacraft.github.io/chatemotes-userscript/chatemotes-userscript.user.js
+// ==/UserScript==
